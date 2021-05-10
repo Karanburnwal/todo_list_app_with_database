1dc8591a20bacc49b68a0922066e014956d0723c
Karanburnwal <48697249+Karanburnwal@users.noreply.github.com>
2021-05-10 12:28:29 +0530

    first commit

---
+++ todo.js
@@ -0,0 +1,78 @@
+const express=require('express')
+const mongoose=require('mongoose')
+const bodyParser=require('body-parser')
+const app = express()
+
+app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.set('view engine', 'ejs')
+
+mongoose.connect("mongodb+srv://dbkaran:Karan@123@cluster0.p32px.mongodb.net/datadb" ,{useNewUrlParser:true, useUnifiedTopology: true});
+
+const listSchema=new mongoose.Schema({
+    name:String
+})
+const List=mongoose.model('List',listSchema);
+
+
+
+var dayName=['sun','mon','tue','wed','thu','fri','sat'];
+
+var items=[
+    {name:"eat"},
+    {name:"food"}
+];
+
+app.get('/',function(req,res){
+    const today=new Date();
+
+    const day=today.getDay();
+    var options={
+        weekday: "short",
+        day: "numeric",
+        month: "long"
+    };
+    var name=today.toLocaleDateString("hi-IN",options)
+    List.find(function(err,lists){
+        if(err){
+            console.log(err);
+        }
+        else{
+            res.render('index',{name:name,newItem:lists});
+        }
+        
+    })
+
+})
+
+
+app.post('/',function(req,res){
+    var item=req.body.newItem;
+    let item_obj={name:item};
+    // items.push(item_obj);
+
+    const list=new List(item_obj);
+    list.save();
+    res.redirect('/');
+})
+
+app.post('/delete',function(req,res){
+    console.log(req.body);
+    List.findByIdAndRemove({_id:req.body.checkbox_id},{useFindAndModify:false},function(err){
+        if(err){
+            console.log(err);
+        }
+        else{
+            console.log("successfully deleted");
+        }
+    })
+    res.redirect('/');
+})
+
+
+app.listen(3000,function(){
+    console.log("server is running");
+})
+
